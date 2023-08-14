caebe6a51a21189bf5020a81b20d8afed0eab36d
vectorNull <jose.rodriguez76@protonmail.com>
2023-08-14 14:31:31 -0700

    Wired up dbContext and created migration

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+namespace ToDoGrpc.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string TodoStatus { get; set; } = "NEW";
+    }
+}
